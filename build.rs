bb43c6ed4cb2933b2bbdd37f5ed2886153bd045a
JulMan <julman@julman.fr>
2026-09-02 01:40:15 +0200

    Updating build.rs Now invoking CMake only when compiling the `gl` feature.

--- build.rs
+++ build.rs
@@ -5,21 +5,24 @@ const EXTENSIONS: &[&str] = &["c", "h", "swift", "modulemap"];
 fn main() {
     input::cargo_cfg_target_os();
 
-    let mut lib = cmake::Config::new(".")
-        .generator("Ninja")
-        .build_target("cpge-native")
-        .build();
+    if cfg!(feature = "gl") {
+        let mut lib = cmake::Config::new(".")
+            .generator("Ninja")
+            .build_target("cpge-native")
+            .build();
 
-    lib.push("build");
+        lib.push("build");
 
-    for entry in walkdir::WalkDir::new("src").into_iter().flatten() {
-        if entry.file_type().is_file() &&
-            entry.path().extension().is_some_and(|ext| EXTENSIONS.iter().any(|x| ext == *x)) {
-            output::rerun_if_changed(entry.path());
+        for entry in walkdir::WalkDir::new("src").into_iter().flatten() {
+            if entry.file_type().is_file() &&
+                entry.path().extension().is_some_and(|ext| EXTENSIONS.iter().any(|x| ext == *x)) {
+                output::rerun_if_changed(entry.path());
+            }
         }
+
+        output::rustc_link_search_kind("native", lib);
+        output::rerun_if_changed("CMakeLists.txt");
     }
 
-    output::rustc_link_search_kind("native", lib);
-    output::rerun_if_changed("CMakeLists.txt");
     output::rerun_if_changed("build.rs");
 }
